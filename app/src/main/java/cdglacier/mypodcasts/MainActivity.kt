7fed731e389f64a7010a279b993743912bdfc7eb
cd-glacier <hyoga0216@gmail.com>
2021-07-08 02:02:27 +0900

    Changable header tab

--- app/src/main/java/cdglacier/mypodcasts/MainActivity.kt
+++ app/src/main/java/cdglacier/mypodcasts/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import cdglacier.mypodcasts.ui.component.HeaderTabRow
 import cdglacier.mypodcasts.ui.theme.MyPodcastsTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +29,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyPodcastsApp() {
+    val navController = rememberNavController()
+    val backstackEntry = navController.currentBackStackEntryAsState()
+    val currentScreen = MyPodcastsScreen.fromRoute(
+        backstackEntry.value?.destination?.route
+    )
+
     Surface(color = MaterialTheme.colors.background) {
-        Text("my podcasts app")
+        Scaffold(
+            topBar = {
+                HeaderTabRow(
+                    allScreens = MyPodcastsScreen.values().toList(),
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
+                    currentScreen = currentScreen
+                )
+            }
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = MyPodcastsScreen.Home.name
+            ) {
+                composable(MyPodcastsScreen.Home.name) {
+                    Text(MyPodcastsScreen.Home.name)
+                }
+
+                composable(MyPodcastsScreen.Podcast.name) {
+                    Text(MyPodcastsScreen.Podcast.name)
+                }
+
+                composable(MyPodcastsScreen.Setting.name) {
+                    Text(MyPodcastsScreen.Setting.name)
+                }
+            }
+        }
     }
 }
 
