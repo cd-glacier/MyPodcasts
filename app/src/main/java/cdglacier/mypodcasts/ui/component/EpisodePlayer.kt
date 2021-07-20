6077eff47ae2b59eb21c471d831b0a3141857b3d
cd-glacier <hyoga0216@gmail.com>
2021-07-21 07:32:41 +0900

    Use custmized exo_player

--- app/src/main/java/cdglacier/mypodcasts/ui/component/EpisodePlayer.kt
+++ app/src/main/java/cdglacier/mypodcasts/ui/component/EpisodePlayer.kt
@@ -1,16 +1,20 @@
 package cdglacier.mypodcasts.ui.component
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.constraintlayout.compose.ConstraintLayout
+import cdglacier.mypodcasts.databinding.ExoPlayerRootBinding
+import com.google.accompanist.coil.rememberCoilPainter
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.ui.PlayerView.SHOW_BUFFERING_ALWAYS
 
 @Composable
@@ -26,24 +30,60 @@ fun EpisodePlayer(
         prepare()
         playWhenReady = true
     }
-
-    AndroidView(
-        factory = {
-            PlayerView(context).apply {
-                player = exoPlayer
-                useArtwork = false
-                controllerShowTimeoutMs = 0
-                controllerHideOnTouch = false
-                controllerAutoShow = true
-                setShowBuffering(SHOW_BUFFERING_ALWAYS)
-            }
-        },
+    ConstraintLayout(
         modifier = Modifier
+            .padding(12.dp)
             .fillMaxWidth()
-            .height(100.dp)
-    )
+    ) {
+        val (imageRef, titleRef, playerRef) = createRefs()
+
+        AndroidViewBinding(
+            factory = ExoPlayerRootBinding::inflate,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .constrainAs(playerRef) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            this.player.apply {
+                player = exoPlayer
+            }
+        }
+
+        Image(
+            painter = rememberCoilPainter(request = imageUrl, fadeIn = true),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(80.dp)
+                .constrainAs(imageRef) {
+                    top.linkTo(parent.top, 12.dp)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                }
+        )
+
+        Text(
+            text = title,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .width(220.dp)
+                .constrainAs(titleRef) {
+                    top.linkTo(parent.top, 12.dp)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(imageRef.end, margin = 18.dp)
+                    end.linkTo(parent.end, 40.dp)
+                }
+        )
+    }
 }
 
+/*
 @Preview
 @Composable
 fun EpisodePlayerPreview() {
@@ -53,3 +93,4 @@ fun EpisodePlayerPreview() {
         mediaUrl = ""
     )
 }
+ */
