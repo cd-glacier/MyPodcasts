29a903d50083cf82c11eb7f87eb0c0a6b311fe5e
cd-glacier <hyoga0216@gmail.com>
2021-08-03 14:21:35 +0900

    Impl input field

--- app/src/main/java/cdglacier/mypodcasts/ui/setting/SubscribedChannelSetting.kt
+++ app/src/main/java/cdglacier/mypodcasts/ui/setting/SubscribedChannelSetting.kt
@@ -4,19 +4,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import cdglacier.mypodcasts.ui.theme.MyPodcastsTheme
 
 @Composable
 fun SubscribedChannelSetting() {
+    // TODO: replace room
+    val (text, setText) = remember { mutableStateOf("") }
+
     Card(
         modifier = Modifier
             .padding(2.dp)
@@ -37,6 +40,46 @@ fun SubscribedChannelSetting() {
                 thickness = 2.dp,
                 startIndent = 8.dp
             )
+
+            SubscribedChannelContent(
+                text = text,
+                onChange = setText
+            )
+        }
+    }
+}
+
+@Composable
+fun SubscribedChannelContent(
+    text: String,
+    onChange: (String) -> Unit
+) {
+    ConstraintLayout(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        val (inputRef, saveButtonRef) = createRefs()
+
+        TextField(
+            value = text,
+            onValueChange = onChange,
+            modifier = Modifier.constrainAs(inputRef) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+            }
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .constrainAs(saveButtonRef) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(inputRef.end)
+                    end.linkTo(parent.end)
+                }
+        ) {
+            Text(text = "SAVE")
         }
     }
 }
